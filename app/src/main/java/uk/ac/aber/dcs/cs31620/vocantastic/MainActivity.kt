5e52f24f4c154fa03fdde119bc01267ceb947644
milkowska <weronikamilka1706@gmail.com>
2022-11-14 00:39:49 +0000

    added icons and basic screen info

--- app/src/main/java/uk/ac/aber/dcs/cs31620/vocantastic/MainActivity.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/vocantastic/MainActivity.kt
@@ -16,7 +16,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import uk.ac.aber.dcs.cs31620.vocantastic.ui.home.HomeScreen
+import uk.ac.aber.dcs.cs31620.vocantastic.ui.list.ViewList
+import uk.ac.aber.dcs.cs31620.vocantastic.ui.navigation.Screen
+import uk.ac.aber.dcs.cs31620.vocantastic.ui.testing.TestScreen
 import uk.ac.aber.dcs.cs31620.vocantastic.ui.theme.VocantasticTheme
+import uk.ac.aber.dcs.cs31620.vocantastic.ui.words.AddWordScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    //Greeting("Android")
+                    CreateNavigation()
                 }
             }
         }
@@ -37,10 +45,26 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+private fun CreateNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) { HomeScreen(navController) }
+        composable(Screen.List.route) { ViewList(navController) }
+        composable(Screen.Test.route) { TestScreen(navController) }
+        composable(Screen.Words.route) { AddWordScreen(navController) }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    VocantasticTheme() {
-       // Greeting("Android")
+    VocantasticTheme(dynamicColor = false) {
+        CreateNavigation()
     }
 }
